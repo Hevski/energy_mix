0ef73620fe511ffd2576cc4b2df17515fddee092
Heather <heathercdavies@gmail.com>
2019-02-20 20:50:36 +0000

    added graph

--- src/app.js
+++ src/app.js
@@ -8,8 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
 			"gchart": GChart
 		},
 		data: {
-      energy: {},
 			genMix: [],
+			chartData: [],
+			chartOptions: {
+				width: 800,
+				height: 240,
+				title: 'Generation Mix for the GB power system',
+				colors: ['#084887', '#f58a07', '#f9ab55', '#f7f5fb', '#909cc2', '#C1BB8F', '#A176BC']
+			},
 			timeFrom: [],
 			timeTo: []
 
@@ -23,10 +29,22 @@ document.addEventListener("DOMContentLoaded", () => {
 					this.timeFrom = result.data.from;
 					this.timeTo = result.data.to;
 				})
+				.then(() => {
+					 this.genChartData()
+				})
+			},
+			genChartData: function(){
+				const chartData = [];
+				chartData[0] = ["fuel", "percentage"];
+				this.genMix.forEach((mix) => {
+					const newData = [mix.fuel, mix.perc];
+					chartData.push(newData);
+				})
+				this.chartData = chartData;
 			}
 		},
 		mounted: function() {
-      this.getEnergy()
+			this.getEnergy()
 		}
 	});
 });
